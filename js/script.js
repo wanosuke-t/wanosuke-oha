fc407e8d8503dc38df7df72f0de414cbe477b2ae
和ノ助（わのすけ） <wanosuke.t@gmail.com>
2025-02-24 10:36:17 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -117,3 +117,20 @@ window.addEventListener("scroll", function () {
     pageTop.classList.remove("is-show");
   }
 });
+
+// フワッと表示
+const intersectionObserver = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-in-view");
+    } else {
+      // entry.target.classList.remove("is-in-view");
+    }
+  });
+});
+
+const inViewItems = document.querySelectorAll(".js-in-view");
+
+inViewItems.forEach(function (inViewItem) {
+  intersectionObserver.observe(inViewItem);
+});
